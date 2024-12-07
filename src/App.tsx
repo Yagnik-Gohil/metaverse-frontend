04de7f5f6a87a640d85dc586ef25b1ea12f1acd9
Yagnik-Gohil <gohilyagnik3@gmail.com>
2024-12-07 08:39:40 +0530

    feat: project setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+
+  return (
+    <div className='h-dvh flex items-center justify-center'>
+      <Button>Hi</Button>
+    </div>
+  )
+}
+
+export default App
